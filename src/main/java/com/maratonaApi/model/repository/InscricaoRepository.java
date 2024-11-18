0351bc7d78589d48c61e495baeaf9bc6979f035e
JoaoV-A01 <victor.joao01101010@gmail.com>
2024-11-18 00:01:45 -0300

    incluindo metodo para listar maratonas abertas por id do corredor

--- src/main/java/com/maratonaApi/model/repository/InscricaoRepository.java
+++ src/main/java/com/maratonaApi/model/repository/InscricaoRepository.java
@@ -1,23 +1,23 @@
 package com.maratonaApi.model.repository;
 
-//import java.util.List;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.maratonaApi.model.Inscricao;
-//import com.maratonaApi.model.Maratona;
+import com.maratonaApi.model.Maratona;
 
 public interface InscricaoRepository extends JpaRepository<Inscricao, Integer> {
 	
-	//Select todas as maratonas com status Aberta com base no id do corredor 
-	//@Query("SELECT m FROM Maratona m JOIN Inscricao i ON m.id_maratona = i.maratona.id_maratona WHERE i.corredor.id_corredor = :idCorredor AND (m.status = 'Aberta para Inscrição' OR m.status = 'Aberta')")
-    //List<Maratona> findMaratonasByAbertas(@Param("idCorredor") int idCorredor);
+	//Select todas as maratonas com status Aberta com base no id do corredor
+	@Query("SELECT m FROM Maratona m JOIN Inscricao i ON m.idMaratona = i.idMaratona WHERE i.idCorredor = :idCorredor AND (m.status = 'Aberta para Inscrição' OR m.status = 'Aberta')")
+    List<Maratona> findMaratonasAbertasByIdCorredor(@Param("idCorredor") int idCorredor);
 	
-	//Lista maratonas concluidas de um determinado corredor
+	//Lista maratonas concluídas de um determinado corredor
 	//@Query("SELECT m FROM Maratona m JOIN Inscricao i ON m.id_maratona = i.maratona.id_maratona WHERE i.corredor.idid_corredor = :idCorredor AND m.status = 'Concluido'")
-    //List<Maratona> findMaratonasByConcluidas(@Param("idCorredor") int idCorredor);
+    //List<Maratona> findMaratonasConcluidasByIdCorredor(@Param("idCorredor") int idCorredor);
 	
 	//Lista corredores de um determinada maratona
 	
