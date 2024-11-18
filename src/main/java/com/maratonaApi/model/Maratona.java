6a843077953c2aece1044d80277d84fb36c959f4
JoaoV-A01 <victor.joao01101010@gmail.com>
2024-11-18 00:01:22 -0300

    usando lombok para os getters e setters

--- src/main/java/com/maratonaApi/model/Maratona.java
+++ src/main/java/com/maratonaApi/model/Maratona.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "maratona")
@@ -15,34 +17,50 @@ public class Maratona {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_maratona")
     private int idMaratona;
-	@Column(name = "criador")
+	@Setter
+    @Getter
     private int criador;
-	@Column(name = "nome")
+	@Setter
+    @Getter
     private String nome;
-	@Column(name = "local")
+	@Setter
+    @Getter
     private String local;
+    @Setter
+    @Getter
     @Column(name = "data_inicio")
     private String dataInicio;
+    @Setter
+    @Getter
     @Column(name = "data_final")
     private String dataFinal;
-    @Column(name = "status")
+    @Setter
+    @Getter
     private String status;
-    @Column(name = "distancia")
+    @Setter
+    @Getter
     private String distancia;
-    @Column(name = "descricao")
+    @Setter
+    @Getter
     private String descricao;
+    @Setter
+    @Getter
     @Column(name = "limite_participantes")
     private int limiteParticipantes;
-    @Column(name = "regras")
+    @Setter
+    @Getter
     private String regras;
-    @Column(name = "valor")
+    @Setter
+    @Getter
     private float valor;
+    @Setter
+    @Getter
     @Column(name = "tipo_terreno")
     private String tipoTerreno;
+    @Setter
+    @Getter
     @Column(name = "clima_esperado")
     private String climaEsperado;
-    @Column(name = "nome_criador")
-    private String nomeCriador;
 
     @Override
 	public String toString() {
@@ -56,7 +74,7 @@ public class Maratona {
         this.idMaratona = id;
     }
 
-    public Maratona(int id, int criador, String nome, String local, String dataInicio, String dataFinal, String status, String distancia, String descricao, String regras, int limiteParticipantes, float valor, String climaEsperado, String tipoTerreno, String nomeCriador) {
+    public Maratona(int id, int criador, String nome, String local, String dataInicio, String dataFinal, String status, String distancia, String descricao, String regras, int limiteParticipantes, float valor, String climaEsperado, String tipoTerreno) {
         this.idMaratona = id;
         this.criador = criador;
         this.nome = nome;
@@ -71,15 +89,6 @@ public class Maratona {
         this.valor = valor;
         this.climaEsperado = climaEsperado;
         this.tipoTerreno = tipoTerreno;
-        this.nomeCriador = nomeCriador;
-    }
-
-    public String getNomeCriador() {
-        return nomeCriador;
-    }
-
-    public void setNomeCriador(String nomeCriador) {
-        this.nomeCriador = nomeCriador;
     }
 
     public int getId() {
@@ -90,107 +99,4 @@ public class Maratona {
         this.idMaratona = id;
     }
 
-    public int getCriador() {
-        return criador;
-    }
-
-    public void setCriador(int criador) {
-        this.criador = criador;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getLocal() {
-        return local;
-    }
-
-    public void setLocal(String local) {
-        this.local = local;
-    }
-
-    public String getDataFinal() {
-        return dataFinal;
-    }
-
-    public void setDataFinal(String data_final) {
-        this.dataFinal = data_final;
-    }
-
-    public String getDataInicio() {
-        return dataInicio;
-    }
-
-    public void setDataInicio(String data_inicio) {
-        this.dataInicio = data_inicio;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getDistancia() {
-        return distancia;
-    }
-
-    public void setDistancia(String distancia) {
-        this.distancia = distancia;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getLimiteParticipantes() {
-        return limiteParticipantes;
-    }
-
-    public void setLimiteParticipantes(int limite_participantes) {
-        this.limiteParticipantes = limite_participantes;
-    }
-
-    public String getRegras() {
-        return regras;
-    }
-
-    public void setRegras(String regras) {
-        this.regras = regras;
-    }
-
-    public float getValor() {
-        return valor;
-    }
-
-    public void setValor(float valor) {
-        this.valor = valor;
-    }
-
-    public String getTipoTerreno() {
-        return tipoTerreno;
-    }
-
-    public void setTipoTerreno(String tipo_terreno) {
-        this.tipoTerreno = tipo_terreno;
-    }
-
-    public String getClimaEsperado() {
-        return climaEsperado;
-    }
-
-    public void setClimaEsperado(String clima_esperado) {
-        this.climaEsperado = clima_esperado;
-    }
 }
